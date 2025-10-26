b98ce261e687fa35983217dfc3e9a875521d2154
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-10-26 15:17:16 +0100

    Pages added

---
+++ Port-man/src/pages/market.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function market(){
+    return(
+        <>
+            <div>
+                <h1>The Market</h1>
+                <p>Get news and analysis on your chossen market, to know where to place your chips.</p>
+            </div>
+        </>
+    )
+}
---
+++ Port-man/src/pages/rates.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function rate(){
+    return(
+        <>
+            <div>
+                <h1>Rates</h1>
+                <p>Set your trading rates and limits on the amount of changes/investments to be made by your AI brooker.</p>
+            </div>
+        </>
+    )
+}
