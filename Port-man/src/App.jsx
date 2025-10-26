3cf0355bf464e4f101a05697d739a198db9d3a49
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-10-26 15:12:15 +0100

    details added

--- Port-man/src/App.jsx
+++ Port-man/src/App.jsx
@@ -1,41 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
-        <h1>Port_Man</h1>
-        <p>An AI agent web app that studies selected shares, stuidies trends, tracks news, stalks investors and provides analysis/purchase advice </p>
-        <p>Your very own Portfolio Manager</p>
+        <h1>Portfilio Manager</h1>
+        <p>Using Ai analysis and predictions of your chossen market.</p>
+        <p>Let us automate research and help yo make the best decisions.</p>
       </div>
-      <div>
-        <p>Graph or practical demenstration animation</p>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
