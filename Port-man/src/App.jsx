a63f375337984bbf76f247cf01db383a4eabe8bc
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-11-02 14:28:39 +0100

    Update

--- Port-man/src/App.jsx
+++ Port-man/src/App.jsx
@@ -1,11 +1,16 @@
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import market from './pages/market'
 
 function App() {
 
 
   return (
     <>
+      <Routes>
+        <Route path='/market' element={<market />} />
+
+      </Routes>
       <div>
         <h1>Portfilio Manager</h1>
         <p>Using Ai analysis and predictions of your chossen market.</p>
